4365cb555905bbe8bbb96b0c978685c2771e926a
Riccardo Gulin <riccardo@nucleode.com>
2023-02-15 12:55:48 +0100

    Implements chat

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import cors from "cors"
 import mongoose from "mongoose"
 import { Server } from "socket.io"
 import { createServer } from "http" // CORE MODULE
+import { newConnectionHandler } from "./socket/index.js"
 
 const expressServer = express()
 const port = process.env.PORT || 3001
@@ -11,6 +12,8 @@ const port = process.env.PORT || 3001
 // ************************************ SOCKET.IO ********************************
 const httpServer = createServer(expressServer)
 const io = new Server(httpServer) // this constructor is expecting to receive an HTTP-SERVER as parameter not an EXPRESS SERVER!!!
+
+io.on("connection", newConnectionHandler) // "connection" is NOT a custom event! This is a socket.io event, triggered every time a new client connects!
 
 // *********************************** MIDDLEWARES *******************************
 
